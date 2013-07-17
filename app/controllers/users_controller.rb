30ae31ee2d63adba52824a85a4d3b139b6d72fa0
vagrant <vagrant@precise64.(none)>
2013-07-17 05:20:32 +0000

    created Signup form and profile ages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    
+ 
+    if @user.save
+      redirect_to @user, notice: "Thank you for signing up for twitter clone!"
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  ##########################
+  private
+    def user_params
+      params.require(:user).permit(:avatar_url, :email, :name, :password, :password_confirmation, :username)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+  before_validation :prep_email
+
+  before_save :create_avatar_url
+
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true, :with => /@/
+  validates :username, uniqueness: true, presence: true
+  validates :name, presence: true
+  
+  private
+ 
+  def prep_email
+    self.email = self.email.strip.downcase if self.email
+  end
+
+  def create_avatar_url
+    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
+
 end
